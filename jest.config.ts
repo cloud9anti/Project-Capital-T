b2b5c47ff764ccae207037d24a01de53a35a3ee6
Ally Summers <ally.summers@outlook.com.au>
2022-10-17 18:42:35 +1100

    Add eslint dependencies, update all non jest dependencies

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from '@jest/types'
+import { getJestProjects } from '@nrwl/jest';
+
+const config: Config.InitialOptions = {
+  projects: getJestProjects(),
+};
+
+export default config;
